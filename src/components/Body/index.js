d4eebe09f7ebb74673ac315b8c5f450300caa97c
doctorbebo <byronrb92@gmail.com>
2020-05-05 00:16:05 -0700

    added navbar body footer components

---
+++ src/components/Body/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./style.css";
+
+function Body ()
+{
+    return (
+        <div>
+            <h1>Hello from Body</h1>
+        </div>
+    )
+}
+
+export default Body;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./style.css";
+
+function Footer ()
+{
+    return (
+        <div>
+            <h1>Hello from Footer</h1>
+        </div>
+    )
+}
+
+export default Footer;
