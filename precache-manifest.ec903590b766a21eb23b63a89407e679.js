1508d45bbd3163d52441ec3f88b5017b2f9a780b
Byron Brown <byronrb92@gmail.com>
2020-10-13 15:09:54 -0400

    Updates

---
+++ precache-manifest.ec903590b766a21eb23b63a89407e679.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff882f34487c7ac6c4e09a0e8ece5713",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2a38490be9ba4b19ca2b",
+    "url": "/portfolio/static/css/main.c9383e66.chunk.css"
+  },
+  {
+    "revision": "f6672ab04f1483cd34b3",
+    "url": "/portfolio/static/js/2.782bfb5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.782bfb5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a38490be9ba4b19ca2b",
+    "url": "/portfolio/static/js/main.418ad876.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "2d43f12619a816b1cad20526e528539c",
+    "url": "/portfolio/static/media/Byron_Resume.2d43f126.pdf"
+  },
+  {
+    "revision": "8fe3b2eeed6fe064273da0a1d443b160",
+    "url": "/portfolio/static/media/Eat_da_Burger.8fe3b2ee.png"
+  },
+  {
+    "revision": "2a688ea4b6301855a88044f70bfdc8a0",
+    "url": "/portfolio/static/media/Fair_Weather_Hiking.2a688ea4.png"
+  },
+  {
+    "revision": "8a86e777a52ab02df7404fa7011ad317",
+    "url": "/portfolio/static/media/Fitness_Tracker.8a86e777.png"
+  },
+  {
+    "revision": "553c81504b96bac4570017d6bf467ddd",
+    "url": "/portfolio/static/media/Note_Taker.553c8150.png"
+  },
+  {
+    "revision": "d1ed0a537383356f3f4d1432b0de773c",
+    "url": "/portfolio/static/media/PWA_Budget_Tracker.d1ed0a53.png"
+  },
+  {
+    "revision": "fc125272b17b60db4f1d62e00378a74d",
+    "url": "/portfolio/static/media/React_Employee_Directory.fc125272.png"
+  },
+  {
+    "revision": "0605685f028930782f69bd6637d497a8",
+    "url": "/portfolio/static/media/profile.0605685f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ca9dce669fe2001a3161a9fd90f41770.js"
+  "/portfolio/precache-manifest.ec903590b766a21eb23b63a89407e679.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
