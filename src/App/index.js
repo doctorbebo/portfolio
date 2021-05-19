187daf403ec5e78852ab50ad7db80b8b410308bd
Byron Brown <byronrb92@gmail.com>
2021-05-19 12:06:18 -0400

    fixed CSS bug

--- src/App/index.js
+++ src/App/index.js
@@ -5,7 +5,7 @@ import Body from "../components/Body";
 
 function App() {
       return (
-        <div className="full black"> 
+        <div className="black"> 
           <Body/>
         </div>
       );
