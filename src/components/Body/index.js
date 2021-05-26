1e2d6c187e2b9db65de4193f13ae835cd62eaa8e
Byron Brown <byronrb92@gmail.com>
2021-05-26 11:28:47 -0400

    Revert "deleted Project Card component"
    This reverts commit 1bbb50c3de21a543e09cf4fef695823a8519867e.

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ProjectCard from "../ProjectCard";
 import PortCards from "../PortCards";
 import cardData from "../PortCards/cardData"
 import Info from "../Info";
---
+++ src/components/ProjectCard/index.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import "./style.css";
+import icon from "./images/IconOne.png" 
+import orbit from "./images/orbit.png";
+import eatDaBurger from "./images/Eat_da_Burger.png";
+import fitnessTracker from "./images/Fitness_Tracker.png";
+import noteTaker from "./images/Note_Taker.png";
+import reactEmployeeDirectory from "./images/React_Employee_Directory.png";
+import pwaBudgetTracker from "./images/PWA_Budget_Tracker.png";
+
+class ProjectCard extends React.Component
+{   
+    constructor (props) {
+        super(props);
+        this.icon_holder = React.createRef()
+        this.randNum = 0;
+        this.backgroundImage = ""
+        this.link = "http//www.google.com";
+    }
+
+
+    MouseEnter = () => 
+    {
+        const ref = this.icon_holder.current;
+        this.randNum = Math.floor(Math.random()*3)+ 1;
+        ref.classList.toggle(`transform-${this.randNum}`);
+        ref.classList.toggle("origin");
+    }
+
+
+    MouseLeave = () => 
+    {
+        const ref = this.icon_holder.current;
+        ref.classList.toggle(`transform-${this.randNum}`);
+        ref.classList.toggle("origin");
+    }
+
+    handleClick = () => 
+    {
+        window.location = this.link
+    }
+
+    render () 
+    {
+
+        switch (this.props.name) {
+            case "Orbit":
+            this.backgroundImage = orbit;
+            this.link = "https://play.google.com/store/apps/details?id=com.DonationGames.Orbit"
+                break;
+            case "Note Taker App":
+            this.backgroundImage = noteTaker;
+            this.link = "https://afternoon-chamber-95392.herokuapp.com/"
+                break;
+            case "React Employee Directory":
+            this.backgroundImage = reactEmployeeDirectory;
+            this.link = "https://salty-springs-52051.herokuapp.com/"
+                break;
+            case "PWA Budget Tracker":
+            this.backgroundImage = pwaBudgetTracker;
+            this.link = "https://still-eyrie-21891.herokuapp.com/"
+                break;
+            case "Eat da Burger":
+            this.backgroundImage = eatDaBurger;
+            this.link = "https://glacial-woodland-82955.herokuapp.com/"
+                break;            
+            case "Fitness Tracker":
+            this.backgroundImage = fitnessTracker;
+            this.link = "https://guarded-eyrie-47968.herokuapp.com/"
+                break;
+            default:
+                console.log(`${this.props.name} could not be found in switch statement.`);
+                break;
+        }
+
+        
+        return (
+        <div className="project-card" onMouseEnter = {this.MouseEnter} onMouseLeave = {this.MouseLeave}>
+            <div className="image">
+                <a href = {this.link}>
+                    <img className="image" alt = " of project" src = {this.backgroundImage}/>
+                </a> 
+            </div>
+            <div className={`icon-holder origin`} ref = {this.icon_holder}>
+                <p className = "text-icon-overlay" >{this.props.name}</p>
+                <img className="icon" alt = " of project" src = {icon}/>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default ProjectCard;
